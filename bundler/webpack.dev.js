b2d211328887c31942c17bae30f5a35c25d1e708
heramb kataru <herambmurthy2005@gmail.com>
2026-03-04 18:37:09 +0530

    new portfolio v2

---
+++ bundler/webpack.dev.js
@@ -0,0 +1,62 @@
+const path = require('path')
+const { merge } = require('webpack-merge')
+const commonConfiguration = require('./webpack.common.js')
+const portFinderSync = require('portfinder-sync')
+
+const infoColor = (_message) =>
+{
+    return `\u001b[1m\u001b[34m${_message}\u001b[39m\u001b[22m`
+}
+
+module.exports = merge(
+    commonConfiguration,
+    {
+        stats: 'errors-warnings',
+        mode: 'development',
+        infrastructureLogging:
+        {
+            level: 'warn',
+        },
+        devServer:
+        {
+            host: 'localhost',
+            port: portFinderSync.getPort(8080),
+            open: true,
+            https: false,
+            allowedHosts: 'all',
+            hot: false,
+            watchFiles: ['src/**', 'static/**'],
+            static:
+            {
+                watch: true,
+                directory: path.join(__dirname, '../static')
+            },
+            client:
+            {
+                logging: 'none',
+                overlay: true,
+                progress: false
+            },
+            setupMiddlewares: (middlewares, devServer) =>
+            {
+                const port = devServer.options.port
+                const https = devServer.options.https ? 's' : ''
+                let localIp = 'localhost'
+                try
+                {
+                    const ip = require('ip')
+                    localIp = ip.address()
+                }
+                catch (e)
+                {
+                    // Fallback if ip.address() fails (e.g. sandbox/network restrictions)
+                }
+                const domain1 = `http${https}://${localIp}:${port}`
+                const domain2 = `http${https}://localhost:${port}`
+                
+                console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
+                return middlewares
+            }
+        }
+    }
+)
